bdb497fc74d355b50ca31df4cdac0a21d3cde712
falimoradi <47635556+falimoradi@users.noreply.github.com>
2021-07-10 16:25:43 +0430

    Update eval_mine.py

--- eval_mine.py
+++ eval_mine.py
@@ -748,7 +748,10 @@ if __name__ == '__main__':
 						spl[1][0] = spl[1][0].strip()
 						tmp = spl[1][0]
 						tmp = tmp[::-1]
-						tmp = find_sim(tmp)[0]
+						if tmp not in dawg:
+							aa = find_sim(tmp)
+							if aa != []
+								tmp = aa[0]
 						spl[1][0] = tmp
 
 						
